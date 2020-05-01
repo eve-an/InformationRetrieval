face7be4f9e6b158a4d0b8c0f6593f903ae1b204
kirschholzfink <64407930+kirschholzfink@users.noreply.github.com>
2020-05-01 14:26:55 +0200

    Add files via upload

---
+++ Project/src/main/java/argssearch/ArgDB.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class ArgDB {
+
+	public ArgDB() {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/argdb", "irargdb",
+					"");
+			Statement prep = conn.createStatement();
+			ResultSet rs = prep.executeQuery("SELECT VERSION()");
+		}
+
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void dbInsert(String string) {
+		//TODO
+	}
+
+}
+
