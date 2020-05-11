b8d6105baf281b6807f1dc84ac48cda8f6a8f063
Ivan <ic43nefy@studserv.uni-leipzig.de>
2020-05-11 18:52:29 +0200

    Add Javadoc

--- Project/src/main/java/argssearch/shared/lemmatization/CoreNlpService.java
+++ Project/src/main/java/argssearch/shared/lemmatization/CoreNlpService.java
@@ -10,28 +10,32 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+/**
+ * A class to make use of the CoreNLP functions.
+ */
 public class CoreNlpService {
 
-    private final Properties properties;
     private final StanfordCoreNLP pipeline;
     private final HashSet<String> stopWords;
     private static final Pattern pattern = Pattern.compile("[a-zA-Z]{2,}");
 
     public CoreNlpService() {
-        properties = new Properties();
+        Properties properties = new Properties();
         properties.setProperty("annotators", "tokenize,ssplit,pos,lemma");
         pipeline = new StanfordCoreNLP(properties);
         stopWords = new HashSet<>();
         initStopWords();
     }
 
+    /**
+     * Reads stop words from a list and put them into a HashSet.
+     */
     private void initStopWords() {
         String path = getClass().getResource("/stopwords.json").getPath();
         try (Reader reader = new FileReader(path)) {
@@ -45,6 +49,12 @@ public class CoreNlpService {
         }
     }
 
+    /**
+     * Lemmatizes a given document. This could be a single sentence or a real document as String.
+     *
+     * @param document document as String
+     * @return A List of lemmatized tokens.
+     */
     public List<String> lemmatize(final String document) {
         CoreDocument doc = new CoreDocument(document);
         pipeline.annotate(doc);
@@ -52,13 +62,24 @@ public class CoreNlpService {
         return doc.tokens().stream()
                 .map(CoreLabel::lemma)
                 .filter(this::checkAgainstStopWords)
+                .map(String::toLowerCase)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Checks if a token is a stop word and matches the regex.
+     * The regex accepts only words with alphabetical characters and a minimum length of 2.
+     *
+     * @param token token to check.
+     * @return true if token matches our conditions.
+     */
     private boolean checkAgainstStopWords(final String token) {
         return !stopWords.contains(token) && pattern.matcher(token).matches();
     }
 
+    /**
+     * Test our lemmatize function with an example.
+     */
     private void test() {
         try {
             String file = Files.readString(Paths.get(getClass().getResource("/test/document.txt").getPath()));
