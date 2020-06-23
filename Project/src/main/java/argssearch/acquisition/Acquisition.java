312af7860840743444141ac3244040f9291900a3
Ivan <iv.cygankov@gmail.com>
2020-06-23 11:08:38 +0200

    add javadoc

--- Project/src/main/java/argssearch/acquisition/Acquisition.java
+++ Project/src/main/java/argssearch/acquisition/Acquisition.java
@@ -9,24 +9,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.concurrent.*;
 
+/**
+ * Handles the loading of JSONs to the Database.
+ */
 public class Acquisition {
     private static final Logger logger = LoggerFactory.getLogger(Acquisition.class);
+    private static final ExecutorService es = Executors.newCachedThreadPool();
 
-    public static void exec(String path, BlockingDeque<JsonArgument> queue, ExecutorService es) {
+    /**
+     * The idea is to have a JsonProducer which reads the JSON files, transforms them to POJOs and put them into a Queue.
+     * The JsonConsumer works in another Thread. It reads the Queue's items and save them into the database.
+     *
+     * @param path  path to JSON. If path is a dir the function will read all the JSONs inside this directory
+     * @param queue working queue of JsonConsumer and JsonProducer
+     */
+    public static void exec(String path, BlockingDeque<JsonArgument> queue) {
         File file = new File(path);
 
         if (file.isDirectory()) {   // Read all files in directory
             try {
-                Files.list(file.toPath()).forEach(json -> read(json.toString(), queue, es));
+                Files.list(file.toPath()).forEach(json -> read(json.toString(), queue));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {    // Read a single file
-            read(path, queue, es);
+            read(path, queue);
         }
     }
 
-    private static void read(String path, BlockingDeque<JsonArgument> queue, ExecutorService es) {
+    private static void read(String path, BlockingDeque<JsonArgument> queue) {
         logger.info("Start reading Json {} and inserting data into database.", path);
         ArgDB.getInstance().executeSqlFile("/database/insertion/temp/create_temp_tables.sql");
 
@@ -37,7 +48,6 @@ public class Acquisition {
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
-
 
         logger.info("Finished reading Json {}", path);
     }
--- Project/src/main/java/argssearch/retrieval/models/vectorspace/DocumentTermMatrix.java
+++ Project/src/main/java/argssearch/retrieval/models/vectorspace/DocumentTermMatrix.java
@@ -3,23 +3,30 @@ package argssearch.retrieval.models.vectorspace;
 import argssearch.indexing.index.Weighter;
 import argssearch.shared.db.ArgDB;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Document Term Representation
+ * Holds a List of Vectors which corresponds to documents.
+ */
+public class DocumentTermMatrix {
+    private final List<Vector> documents;
 
-public class DocumentTerm {
-    private List<Vector> documents;
-
-    public DocumentTerm() {
+    public DocumentTermMatrix() {
         documents = new ArrayList<>();
     }
 
-
+    /**
+     * Read all Documents into this matrix and compute their token's weights.
+     *
+     * @param weighter Weighter to compute the token's weight.
+     */
     public void addDocuments(final Weighter weighter) {
+        // docId : ARRAY {token1, token2...}
         ResultSet rs = ArgDB.getInstance().query("SELECT a.argid, array_agg(t.token) FROM argument a " +
                 "LEFT JOIN argument_index ai on a.argid = ai.argid " +
                 "LEFT JOIN token t on ai.tid = t.tid " +
@@ -29,13 +36,13 @@ public class DocumentTerm {
         try {
             while (rs.next()) {
                 int argid = rs.getInt(1);
-                Vector vector = new Vector(weighter.getArgumentCount(), argid);
+                Vector vector = new Vector(weighter.getArgumentCount() + weighter.getDiscussionCount() + weighter.getPremiseCount(), argid);
                 String[] docTerms = (String[]) rs.getArray(2).getArray();
 
                 for (String docTerm : docTerms) {
                     int termIndex = ArgDB.getInstance().getIndexOfTerm(docTerm) - 1;
                     if (termIndex >= 0) {
-                        vector.set(termIndex, weighter.getArgumentIdf(docTerm, argid));
+                        vector.set(termIndex, weighter.getArgumentTfIdf(docTerm, argid));
                     }
                 }
 
@@ -46,16 +53,24 @@ public class DocumentTerm {
         }
     }
 
+    /**
+     * Computes the similarity between the query and all documents from this matrix
+     * and returns a List with ranked Documents sort after their ranking.
+     *
+     * @param query query Vector
+     * @return List of ranked {@link Document} sort after their ranking.
+     */
     public List<Document> processQuery(Vector query) {
         List<Document> docs = new ArrayList<>();
         for (Vector document : documents) {
-            double rank = VectorMath.getCosineSimilarity(query, document);
+            double similarity = VectorMath.getCosineSimilarity(query, document);
 
-            if (rank > 0) {
-                docs.add(new Document(document.getDocId(), rank));    // ID = Database ID!
+            // When similarity is less than 0 the two documents aren't similar
+            if (similarity > 0) {
+                docs.add(new Document(document.getDocId(), similarity));    // ID = Database ID!
             }
         }
-        Collections.sort(docs);
+        Collections.sort(docs); // Sort after similarity descending
 
         return docs;
     }
